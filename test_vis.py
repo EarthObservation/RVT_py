5cbd2b16e5acb5fc7f96544fc9fa88b477249b37
Žiga <zm8597@student.uni-lj.si>
2020-09-22 10:48:21 +0200

    rename, close datasets

--- test_vis.py
+++ test_vis.py
@@ -23,6 +23,8 @@ def test_slope_aspect(input_dem_path, output_path, ve_factor=1, output_units="de
     profile.update(dtype='float64')
     output_slope_dataset = rio.open(output_path, "w", **profile)
     output_slope_dataset.write(np.array([slope_arr]))
+    input_dem_dataset.close()
+    output_slope_dataset.close()
 
 
 def test_hillshade(input_dem_path, output_path, sun_azimuth=315, sun_elevation=35):
@@ -32,14 +34,16 @@ def test_hillshade(input_dem_path, output_path, sun_azimuth=315, sun_elevation=3
     y_res = -t[4]
     input_dem_arr = input_dem_dataset.read()[0]
 
-    hillshading_arr = rvt.vis.hillshade(dem=input_dem_arr, resolution_x=x_res,
-                                        resolution_y=y_res, sun_azimuth=sun_azimuth, sun_elevation=sun_elevation,
-                                        is_padding_applied=False)
-    hillshading_arr = hillshading_arr.astype('float64')
+    hillshade_arr = rvt.vis.hillshade(dem=input_dem_arr, resolution_x=x_res,
+                                      resolution_y=y_res, sun_azimuth=sun_azimuth, sun_elevation=sun_elevation,
+                                      is_padding_applied=False)
+    hillshade_arr = hillshade_arr.astype('float64')
     profile = input_dem_dataset.profile
     profile.update(dtype='float64')
-    output_hillshading_dataset = rio.open(output_path, "w", **profile)
-    output_hillshading_dataset.write(np.array([hillshading_arr]))
+    output_hillshade_dataset = rio.open(output_path, "w", **profile)
+    output_hillshade_dataset.write(np.array([hillshade_arr]))
+    input_dem_dataset.close()
+    output_hillshade_dataset.close()
 
 
 def test_multi_hillshade(input_dem_path, output_path, nr_directions=16, sun_elevation=35):
@@ -48,17 +52,18 @@ def test_multi_hillshade(input_dem_path, output_path, nr_directions=16, sun_elev
     x_res = t[0]
     y_res = -t[4]
     input_dem_arr = input_dem_dataset.read()[0]
-    multi_hillshading_arr = rvt.vis.multi_hillshade(dem=input_dem_arr, resolution_x=x_res,
-                                                    resolution_y=y_res, nr_directions=nr_directions,
-                                                    sun_elevation=sun_elevation,
-                                                    is_padding_applied=False)
-    multi_hillshading_arr = multi_hillshading_arr.astype('float64')
+    multi_hillshade_arr = rvt.vis.multi_hillshade(dem=input_dem_arr, resolution_x=x_res,
+                                                  resolution_y=y_res, nr_directions=nr_directions,
+                                                  sun_elevation=sun_elevation,
+                                                  is_padding_applied=False)
+    multi_hillshade_arr = multi_hillshade_arr.astype('float64')
     profile = input_dem_dataset.profile
     profile.update(dtype='float64')
     profile.update(count=16)
-    output_multi_hillshading_dataset = rio.open(output_path, "w", **profile)
-    output_multi_hillshading_dataset.write(multi_hillshading_arr)
-
+    output_multi_hillshade_dataset = rio.open(output_path, "w", **profile)
+    output_multi_hillshade_dataset.write(multi_hillshade_arr)
+    input_dem_dataset.close()
+    output_multi_hillshade_dataset.close()
 
 def test_slrm(input_dem_path, output_path):
     input_dem_dataset = rio.open(input_dem_path)
@@ -68,9 +73,10 @@ def test_slrm(input_dem_path, output_path):
     slrm_arr = slrm_arr.astype('float64')
     profile = input_dem_dataset.profile
     profile.update(dtype='float64')
-    output_slrm_arr_dataset = rio.open(output_path, "w", **profile)
-    output_slrm_arr_dataset.write(np.array([slrm_arr]))
-
+    output_slrm_dataset = rio.open(output_path, "w", **profile)
+    output_slrm_dataset.write(np.array([slrm_arr]))
+    input_dem_dataset.close()
+    output_slrm_dataset.close()
 
 def test_sky_view_factor(input_dem_path, output_svf_path, output_asvf_path, output_opns_path):
     input_dem_dataset = rio.open(input_dem_path)
@@ -88,18 +94,22 @@ def test_sky_view_factor(input_dem_path, output_svf_path, output_asvf_path, outp
     profile.update(dtype='float64')
     output_svf = rio.open(output_svf_path, "w", **profile)
     output_svf.write(np.array([svf_arr]))
+    output_svf.close()
 
     asvf_arr = dict_svf_asvf_opns["asvf"]
     asvf_arr = asvf_arr.astype('float64')
 
     output_asvf = rio.open(output_asvf_path, "w", **profile)
     output_asvf.write(np.array([asvf_arr]))
+    output_asvf.close()
 
     opns_arr = dict_svf_asvf_opns["opns"]
     opns_arr = opns_arr.astype('float64')
 
     output_opns = rio.open(output_opns_path, "w", **profile)
     output_opns.write(np.array([opns_arr]))
+    output_opns.close()
+    input_dem_dataset.close()
 
 
 def test_sky_illumination(input_dem_path, output_path):
@@ -114,6 +124,8 @@ def test_sky_illumination(input_dem_path, output_path):
     profile.update(dtype='float64')
     output_skyilum = rio.open(output_path, "w", **profile)
     output_skyilum.write(np.array([skyillumination_arr]))
+    output_skyilum.close()
+    input_dem_dataset.close()
 
 
 def test_local_dominance(input_dem_path, output_path):
@@ -125,6 +137,8 @@ def test_local_dominance(input_dem_path, output_path):
     profile.update(dtype='float64')
     output_localdom = rio.open(output_path, "w", **profile)
     output_localdom.write(np.array([localdominance_arr]))
+    output_localdom.close()
+    input_dem_dataset.close()
 
 ###
 
@@ -150,5 +164,3 @@ def test_local_dominance(input_dem_path, output_path):
 
 # test_local_dominance(input_dem_path=r"D:\RVT_py\test\TM1_564_146.tif",
 #                      output_path=r"D:\RVT_py\test\TM1_564_146_test_localdom.tif")
-
-
