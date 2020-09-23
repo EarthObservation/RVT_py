7f3d7ddf9f22d0a06a7be71540b449f1198bbc7c
Žiga <zm8597@student.uni-lj.si>
2020-09-23 12:08:50 +0200

    changed test file name

--- test_default.py
+++ test_default.py
@@ -7,14 +7,14 @@ default = rvt.default.DefaultValues()
 default.read_default_from_file("D:\RVT_py\RVT_py\settings\default_settings.txt")
 
 # save specific visualization function
-default.save_hillshade(r"D:\RVT_py\test\test.tif")
-default.save_slope(r"D:\RVT_py\test\test.tif")
-default.save_multi_hillshade(r"D:\RVT_py\test\test.tif")
-default.save_slrm(r"D:\RVT_py\test\test.tif")
-default.save_sky_view_factor(r"D:\RVT_py\test\test.tif", save_svf=True, save_asvf=True, save_opns=True)
-default.save_neg_opns(r"D:\RVT_py\test\test.tif")
+default.save_hillshade(r"D:\RVT_py\test\TM1_564_146.tif")
+default.save_slope(r"D:\RVT_py\test\TM1_564_146.tif")
+default.save_multi_hillshade(r"D:\RVT_py\test\TM1_564_146.tif")
+default.save_slrm(r"D:\RVT_py\test\TM1_564_146.tif")
+default.save_sky_view_factor(r"D:\RVT_py\test\TM1_564_146.tif", save_svf=True, save_asvf=True, save_opns=True)
+default.save_neg_opns(r"D:\RVT_py\test\TM1_564_146.tif")
 #default.save_sky_illumination(r"D:\RVT_py\test\test.tif")
-default.save_local_dominance(r"D:\RVT_py\test\test.tif")
+default.save_local_dominance(r"D:\RVT_py\test\TM1_564_146.tif")
 
 # save all visualization functions
-default.save_visualizations(r"D:\RVT_py\test\test.tif", sav_sky_illumination=False)
+default.save_visualizations(r"D:\RVT_py\test\TM1_564_146.tif", sav_sky_illumination=False)
