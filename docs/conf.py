0a34d23e9cc5ca957c57463ff10c05bcb304ec43
KrOstir <krostir@gmail.com>
2020-10-12 18:06:20 +0200

    Update docs for Read the Docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,8 @@ author = 'ZRC SAZU and University of Ljubljana'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
