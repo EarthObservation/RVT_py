59476ad72faab24b30098bbbba4f8f3cbc740565
Žiga <zm8597@student.uni-lj.si>
2020-11-12 13:15:41 +0100

    slrm written in numpy

--- rvt/vis.py
+++ rvt/vis.py
@@ -309,8 +309,22 @@ def slrm(dem,
     dem[dem > 2000] = np.float64(np.NaN)
 
     # mean filter
-    slrm_out = dem - scipy.ndimage.uniform_filter(dem, mode='nearest', size=radius_cell * 2)
-
+    nr_rolls = int(radius_cell)  # number of rools in each direction
+    dem_padded = np.pad(array=dem, pad_width=nr_rolls, mode="edge")  # padding
+    slrm_out = np.copy(dem_padded)
+    for i_y_roll in range(nr_rolls):
+        roll = i_y_roll + 1  # y direction roll
+        slrm_out += np.roll(np.copy(dem_padded), roll, axis=0)  # roll positive direction
+        slrm_out += np.roll(np.copy(dem_padded), -roll, axis=0)  # roll negative direction
+    y_rolls_sum = np.copy(slrm_out)  # sum of all rolls in y direction
+    for i_x_roll in range(nr_rolls):  # x direction roll
+        roll = i_x_roll + 1
+        slrm_out += np.roll(np.copy(y_rolls_sum), roll, axis=1)  # roll positive direction
+        slrm_out += np.roll(np.copy(y_rolls_sum), -roll, axis=1)  # roll negative direction
+    del y_rolls_sum
+    slrm_out = slrm_out / ((2*nr_rolls+1)**2)  # calculate mean radius_cell*radius_cell
+    slrm_out = slrm_out[nr_rolls:-nr_rolls, nr_rolls:-nr_rolls]
+    slrm_out = dem - slrm_out
     return slrm_out
 
 
@@ -939,93 +953,3 @@ def local_dominance(dem,
     local_dom_out = local_dom_out / norma
 
     return local_dom_out
-
-# If we don't fix morp_shade we could use adams_shadows function (for that we would need to use numba)
-# https://github.com/jacobdadams/general_scripts/blob/master/raster_chunk_processing.py
-# import numba
-#
-#
-# @numba.jit(nopython=True)
-# def adams_shadows(in_array, az, alt, res, overlap, nodata=-1):
-#     # Rows = i = y values, cols = j = x values
-#     rows = in_array.shape[0]
-#     cols = in_array.shape[1]
-#     shadow_array = np.ones(in_array.shape)  # init to 1 (not shadowed), change to 0 if shadowed
-#     max_elev = np.max(in_array)
-#
-#     az = 90. - az  # convert from 0 = north, cw to 0 = east, ccw
-#
-#     azrad = az * np.pi / 180.
-#     altrad = alt * np.pi / 180.
-#     delta_j = np.cos(azrad)
-#     delta_i = -1. * np.sin(azrad)
-#     tanaltrad = np.tan(altrad)
-#
-#     mult_size = 1
-#     max_steps = 600
-#
-#     already_shadowed = 0
-#
-#     # precompute idx distances
-#     distances = []
-#     for d in range(1, max_steps):
-#         distance = d * res
-#         step_height = distance * tanaltrad
-#         i_distance = delta_i * d
-#         j_distance = delta_j * d
-#         distances.append((step_height, i_distance, j_distance))
-#
-#     # Only compute shadows for the actual chunk area in a super_array
-#     # We don't care about the overlap areas in the output array, they just get
-#     # overwritten by the nodata value
-#     if overlap > 0:
-#         y_start = overlap - 1
-#         y_end = rows - overlap
-#         x_start = overlap - 1
-#         x_end = cols - overlap
-#     else:
-#         y_start = 0
-#         y_end = rows
-#         x_start = 0
-#         x_end = cols
-#
-#     for i in range(y_start, y_end):
-#         for j in range(x_start, x_end):
-#
-#             point_elev = in_array[i, j]  # the point we want to determine if in shadow
-#
-#             for step in range(1, max_steps):  # start at a step of 1- a point cannot be shadowed by itself
-#
-#                 # No need to continue if it's already shadowed
-#                 if shadow_array[i, j] == 0:
-#                     already_shadowed += 1
-#                     # print("shadow break")
-#                     break
-#
-#                 critical_height = distances[step - 1][0] + point_elev
-#
-#                 # idx_i/j are indices of array corresponding to current position + y/x distances
-#                 idx_i = int(round(i + distances[step - 1][1]))
-#                 idx_j = int(round(j + distances[step - 1][2]))
-#
-#                 in_bounds = idx_i >= 0 and idx_i < rows and idx_j >= 0 and idx_j < cols
-#                 in_height = critical_height < max_elev
-#
-#                 if in_bounds and in_height:
-#                     next_elev = in_array[idx_i, idx_j]
-#                     # Bail out if we hit a nodata area
-#                     if next_elev == nodata or next_elev == np.nan:
-#                         break
-#
-#                     if next_elev > point_elev and next_elev > critical_height:
-#                         shadow_array[i, j] = 0
-#
-#                         # set all array indices in between our found shadowing index and the source index to shadowed
-#                         for step2 in range(1, step):
-#                             i2 = int(round(i + distances[step2 - 1][1]))
-#                             j2 = int(round(j + distances[step2 - 1][2]))
-#                             shadow_array[i2, j2] = 0
-#
-#                         break  # We're done with this point, move on to the next
-#
-#     return shadow_array
